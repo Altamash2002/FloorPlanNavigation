9e926af0c6daf57d8c4aea9379fc25b62f113557
Altamash <altamashshaikh_ce_2020@ltce.in>
2023-12-09 11:17:03 +0530

    basic svg floor plan navigation

--- navigation/views.py
+++ navigation/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Place , Teacher
 
 # Create your views here.
 
 def home(request):
-    return render(request , "index.html")
+    # place1 = Place(name = 'name1' , floor = 1)
+    # teacher = Teacher(name = 'teacher1' , place = 'name1')
+
+    # place1.save()
+    # teacher.save()
+    places = Place.objects.all()
+    return render(request, "index.html", {"places": places})
 
 def navigate(request):
-    return render(request , "navigation.html")
+
+    room = request.GET.get('To')
+
+    return render(request , "navigation.html" , {"destination": room})

b61d1019cf555a73e3fe4864358f9fec6d03d914
Altamash <altamashshaikh_ce_2020@ltce.in>
2023-12-14 11:55:13 +0530

    dijktras algorithm path generating

--- navigation/views.py
+++ navigation/views.py
@@ -15,6 +15,7 @@ def home(request):
 
 def navigate(request):
 
-    room = request.GET.get('To')
+    pfrom = request.GET.get('From')
+    to = request.GET.get('To')
 
-    return render(request , "navigation.html" , {"destination": room})
+    return render(request , "navigation1.html" , {"i": pfrom , "j":to})

59a531ed0ada6889ca5b42bb13cfac81551bff27
Altamash Shaikh <altamash0045@gmail.com>
2024-03-11 12:19:08 +0530

    svg Page changes

--- floorplan/settings.py
+++ floorplan/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'tenurdu/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'navigation/static')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'tenurdu/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'navigation/media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
